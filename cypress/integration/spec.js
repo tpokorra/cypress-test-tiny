452f55d2e2e8889202d7568a2ada1fec3ccf25ba
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-12-14 04:32:58 +0100

    trying to reproduce an error with typing into input field with cypress 3.8.0 but it does seem to work in this test case!

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,13 @@
-describe('page', () => {
-  it('works', () => {})
+describe('typetest', () => {
+  // fails:
+  it('typing does not finish', () => {
+       cy.server()
+       cy.visit('/')
+       cy.get('input[name="firstname"]')
+         .should('have.value', "John")
+         .clear()
+         .type('Albrecht')
+         .wait(1000)
+         .should('have.value', "Albrecht")
+  })
 })
